d377bea4cfd0b617927cec14a63b2cd955df7ac9
BorisMilkman <borismilkman@gmail.com>
2015-12-18 22:06:30 +0300

    Update Grid3d.cs

--- Grid3d.cs
+++ Grid3d.cs
@@ -1,3 +1,28 @@
 public class Grid3d {
+  public static List<Grid3d> list; // or ArrayList
   
+  public static const int tileSize = 1;
+  
+  public static const int pathFound = 1;
+  public static const int pathNotFound = 2;
+  
+  public List<List<List<GridNode3d>>> nodes;
+  public List<GridNode> openedList;
+  public List<GridNode> closedList;
+  public List<GridNode> finalPath;
+  
+  private Vector3 start, end;
+  
+  string name;
+  public Vector3 size;
+  
+  /** INITIALISING */
+  
+  /** CREATING AND SETTING */
+  
+  /** PATHFINDING */
+  
+  /** GETTERS / SETTERS */
+  
+  /** DISPOSING / RESETTING */
 }
