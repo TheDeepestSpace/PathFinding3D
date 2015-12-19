114a6354836a3b8010092e301c714b5e4ce01318
BorisMilkman <borismilkman@gmail.com>
2015-12-19 19:56:22 +0300

    Create Tool.cs

---
+++ Unity3d/Scripts/Tool.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Tool {
+
+	public static Vector3 getSceneCoordinates(Vector3 gridCoordinates) {
+        return new Vector3(
+            gridCoordinates.x * Grid.tileSize + Grid.tileSize / 2,
+            gridCoordinates.y * Grid.tileSize + Grid.tileSize / 2,
+            gridCoordinates.z * Grid.tileSize + Grid.tileSize / 2
+        );
+    }
+}
