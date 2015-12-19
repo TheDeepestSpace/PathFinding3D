f099b0cb21d7e061626f1209082be0f0df3544a8
BorisMilkman <borismilkman@gmail.com>
2015-12-19 19:52:25 +0300

    finished

---
+++ Unity3d/Scripts/Grid/GridNode.cs
@@ -0,0 +1,107 @@
+using UnityEngine;
+
+public class GridNode {
+    public Vector3 gridCoordinates;
+
+    string gridName;
+    public Type type;
+
+    public GridNode parent;
+    public int F, G, H;
+
+    public GameObject model;
+
+  
+    public enum Type {
+        BLANK,
+        START,
+        END,
+        BLOCK
+    }
+
+    /** INITIALISING */
+
+    void initialise() {
+        this.gridCoordinates = new Vector3();
+        this.gridName = "n/a";
+        this.type = Type.BLANK;
+
+        this.parent = null;
+        this.F = this.G = this.H = 0;
+    }
+
+    /** CREATING AND SETTING */
+
+    public GridNode(string gridName, Vector3 gridCoordinates) {
+        this.initialise();
+        this.setUp(gridName, gridCoordinates);
+    }
+
+    void setUp(string gridName, Vector3 gridCoordinates) {
+        this.gridName = gridName;
+        this.gridCoordinates.x = gridCoordinates.x;
+        this.gridCoordinates.y = gridCoordinates.y;
+        this.gridCoordinates.z = gridCoordinates.z;
+
+        this.createModel();
+    }
+
+    void createModel() {
+        this.model = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        this.model.transform.position = Tool.getSceneCoordinates(this.gridCoordinates);
+        this.model.GetComponent<MeshRenderer>().enabled = false;
+    }
+
+    /** PATHFINDING */
+
+    public void calculateValues(GridNode parentNode, GridNode endNode) {
+        this.parent = parentNode;
+
+        double xDistance = Mathf.Abs(this.gridCoordinates.x - this.parent.gridCoordinates.x);
+        double yDistance = Mathf.Abs(this.gridCoordinates.y - this.parent.gridCoordinates.y);
+        double zDistance = Mathf.Abs(this.gridCoordinates.z - this.parent.gridCoordinates.z);
+
+        if (this.parent != null) {
+            if (xDistance != 0 && yDistance != 0 && zDistance != 0) {
+                this.G = this.parent.G + 17;
+            } else if (
+                (xDistance != 0 && yDistance != 0) ||
+                (xDistance != 0 && zDistance != 0) ||
+                (yDistance != 0 && zDistance != 0)
+              ) {
+                this.G = this.parent.G + 14;
+            } else {
+                this.G = this.parent.G + 10;
+            }
+        }
+
+        this.H = (int)xDistance + (int)yDistance + (int)zDistance;
+        this.F = this.G = this.H;
+    }
+
+    /** UPDATE */
+
+    public void updateModel() {
+        if (this.type == Type.BLANK) {
+            this.model.GetComponent<MeshRenderer>().enabled = false;
+        }else {
+            this.model.GetComponent<MeshRenderer>().enabled = true;
+            if (this.type == Type.BLOCK) {
+                this.model.GetComponent<Renderer>().material.color = Color.gray;
+            }
+            if (this.type == Type.START) {
+                this.model.GetComponent<Renderer>().material.color = Color.green;
+            }
+            if (this.type == Type.END) {
+                this.model.GetComponent<Renderer>().material.color = Color.red;
+            }
+        }
+    }
+
+    /** DISPOSING / RESETTING */
+
+    public void reset() {
+        this.F = this.G = this.H = 0;
+        this.parent = null;
+    }
+}
