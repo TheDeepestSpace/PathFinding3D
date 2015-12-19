08e09ed73f3c3b7f724b57cbc39ea17986427cd6
BorisMilkman <borismilkman@gmail.com>
2015-12-19 14:12:50 +0300

    Update CameraSetup.cs

--- Unity3d/Scripts/Camera/CameraSetup.cs
+++ Unity3d/Scripts/Camera/CameraSetup.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class CameraSetup : MonoBehaviour {
 
 	void Start () {
-    gameObject.transform.position = new Vector3(30, 30, 30);
-    gameObject.transform.Rotate(new Vector3(35, 225, 0));
+    		gameObject.transform.position = new Vector3(30, 30, 30);
+    		gameObject.transform.Rotate(new Vector3(35, 225, 0));
 	}
 }
